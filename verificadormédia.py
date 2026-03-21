f10101c88436821939ad979c98b04710001d5664
Luskzinn <lucasviniciussilv@gmail.com>
2026-03-20 22:44:06 -0300

    calculadora de média escolar

---
+++ verificadormédia.py
@@ -0,0 +1,50 @@
+#Levantamento de quantos alunos ficaram em cada situação.
+aprovados = 0
+recuperacao = 0
+reprovados = 0 
+
+#Criando um loop para escolher quantos alunos terão as médias avaliadas
+
+rep = int(input('Quantos Alunos terão a média avaliada? '))
+
+for i in range(rep):
+
+#Receber Notas Dos ALunos
+
+ n1 = float(input('Primeira Nota:  '))
+ while n1 <0 or n1 >10:
+    print('digite uma nota válida')
+    n1 = float(input('Primeira Nota:  '))
+ n2 = float(input('Segunda Nota:  '))
+ while n2 <0 or n2 >10:
+    print('digite uma nota válida')
+    n2 = float(input('Segunda Nota:  '))
+ n3 = float(input('Terceira Nota:  '))
+ while n3 <0 or n3 >10:
+    print('digite uma nota válida')
+    n3 = float(input('Terceira Nota:  '))
+
+#Declarar a variavel média e fazer o calculo da mesma
+ media = (n1 + n2 + n3) / 3 
+
+#Confirmar conforme a média se o aluno foi aprovado, reprovado ou está de recuperação
+ if media >= 7:
+    print(f'A média foi {media:.2f}, aluno aprovado.')
+    aprovados = aprovados + 1
+
+ elif 5<= media <7:
+    print(f'A média foi {media:.2f}, aluno fará recuperção.')
+    recuperacao = recuperacao + 1
+
+ else:
+    print(f'A média foi {media:.2f}, aluno reprovado.')
+    reprovados = reprovados + 1
+
+
+#Mostra quantos alunos ficaram em cada situação
+print('Alunos aprovados: ', aprovados)
+print('Alunos de recuperação: ', recuperacao)
+print('Alunos reprovados: ', reprovados)
+  
+      
+
