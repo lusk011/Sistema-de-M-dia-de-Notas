840f851758c30135f09f97aa67af37f0920a8f44
lusk011 <168434014+lusk011@users.noreply.github.com>
2026-03-25 17:17:13 -0300

    Track average scores for three evaluations
    Added calculations for average scores of three evaluations.

--- verificadormédia.py
+++ verificadormédia.py
@@ -2,6 +2,9 @@
 aprovados = 0
 recuperacao = 0
 reprovados = 0 
+ava1 = 0
+ava2 = 0
+ava3 = 0
 
 #Criando um loop para escolher quantos alunos terão as médias avaliadas
 
@@ -15,14 +18,17 @@ for i in range(rep):
  while n1 <0 or n1 >10:
     print('digite uma nota válida')
     n1 = float(input('Primeira Nota:  '))
+ ava1 = ava1 + n1
  n2 = float(input('Segunda Nota:  '))
  while n2 <0 or n2 >10:
     print('digite uma nota válida')
     n2 = float(input('Segunda Nota:  '))
+ ava2 = ava2 + n2
  n3 = float(input('Terceira Nota:  '))
  while n3 <0 or n3 >10:
     print('digite uma nota válida')
     n3 = float(input('Terceira Nota:  '))
+ ava3 = ava3 + n3
 
 #Declarar a variavel média e fazer o calculo da mesma
  media = (n1 + n2 + n3) / 3 
@@ -41,10 +47,17 @@ for i in range(rep):
     reprovados = reprovados + 1
 
 
+
+
 #Mostra quantos alunos ficaram em cada situação
 print('Alunos aprovados: ', aprovados)
 print('Alunos de recuperação: ', recuperacao)
 print('Alunos reprovados: ', reprovados)
+#Média de nota de cada avaliação
+print('Média da avaliação 1:', format(ava1 / rep, '.2f'))
+print('Média da avaliação 2:', format(ava2 / rep, '.2f'))
+print('Média da avaliação 3:', format(ava3 / rep, '.2f'))
+
   
-      
+
 
